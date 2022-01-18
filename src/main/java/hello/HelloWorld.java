d754d2c2a977412d73118accfaac0fb3a044f704
Shohag Rana <shohagrana64@gmail.com>
2022-01-18 15:14:37 +0600

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 		  System.out.println("The current local time is: " + currentTime);
 	          System.out.println("My name is Shohag Rana and this is my test application :)");
 	          System.out.println("No more fear, Siaful bhai is here.");
-	          System.out.println("Example Change");
+	          System.out.println("Exaple Change");
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
